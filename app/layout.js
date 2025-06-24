816bbb2318b4f10afac44bc23d6ade096573e541
Mihir Jataniya <mihirjataniya1612@gmail.com>
2025-06-24 20:50:56 +0530

    added tags

--- app/layout.js
+++ app/layout.js
@@ -23,6 +23,19 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="s-fqZXZzhxCboLfhN4WNtXWKmt5I-6zzS5GC3DaQdPo" />
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PTXZPFGS');
+          `,
+          }}
+        />
       </head>
       <body
         className={`${montserrat.className} antialiased w-full relative mx-auto flex flex-col`}
@@ -33,6 +46,8 @@ export default function RootLayout({ children }) {
           {children}
         </div>
         <Footer />
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PTXZPFGS"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
       </body>
     </html>
   );
